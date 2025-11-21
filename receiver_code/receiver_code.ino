697a7d76539b2041c1198e95e8b2fdc2a017619a
TonyPansera <tonypansera9@gmail.com>
2025-11-21 09:39:35 +0100

    web files

--- receiver_code/receiver_code.ino
+++ receiver_code/receiver_code.ino
@@ -1,30 +1,59 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <LittleFS.h>
 
 const char* ssid = "Heltec_AP_Tony";
 const char* password = "12345678";
 
 WebServer server(80);
 
-// Exemple de valeur à afficher
+// Exemple de valeur à afficher (utilisé si tu veux ajouter un endpoint JSON)
 float sensorValue = 42.7;
 
+// --- Handlers pour les fichiers ---
 void handleRoot() {
-  String page = "<html><body><h1>Valeurs Heltec</h1>";
-  page += "<p>Valeur = " + String(sensorValue) + "</p>";
-  page += "</body></html>";
-  server.send(200, "text/html", page);
+  File file = LittleFS.open("../web/index.html", "r");
+  if (!file) {
+    server.send(500, "text/plain", "index.html not found");
+    return;
+  }
+
+  server.streamFile(file, "text/html");
+  file.close();
+}
+
+void handleCSS() {
+  File file = LittleFS.open("../web/style.css", "r");
+  if (!file) {
+    server.send(404, "text/plain", "style.css not found");
+    return;
+  }
+
+  server.streamFile(file, "text/css");
+  file.close();
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Mode point d'accès
-  WiFi.softAP(ssid, password);
+  // --- Mount LittleFS ---
+  if (!LittleFS.begin(true)) {
+    Serial.println("LittleFS mount failed");
+    while (true) { delay(1000); }
+  }
+  Serial.println("LittleFS mounted OK");
 
-  // Endpoints HTTP
+  // --- WiFi AP ---
+  WiFi.softAP(ssid, password);
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
+
+  // --- Routes ---
   server.on("/", handleRoot);
+  server.on("/style.css", handleCSS);
+
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
